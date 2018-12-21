9b52002c9704232878b3a9e22484079c97d69330
Mati Green <mati.green.13@gmail.com>
2018-12-21 14:20:52 +0200

    3

--- configure.sh
+++ configure.sh
@@ -1,16 +1,37 @@
-CONF_FOLDER=`dirname $BASH_SOURCE`
+case "$1" in
+    all)
+        cp -r bin $HOME
+        for i in `ls $HOME/bin`; do chmod +x $HOME/bin/$i; done;
+        cp -r .tmux $HOME
+        cp .tmux.conf $HOME
+        cp .vimrc $HOME
+        cp .bashrc $HOME
+        cp .i3 $HOME/.config/i3/config
+        git clone https://github.com/shikherverma/i3lock-multimonitor.git
+        cp -r i3lock-multimonitor ~/.i3
+        chmod +x ~/.i3/i3lock-multimonitor/lock
+        apt install scrot
+        ;;
+    bin)
+        cp -r bin $HOME
+        for i in `ls $HOME/bin`; do chmod +x $HOME/bin/$i; done;
+        ;;
+    tmux)
+        cp -r .tmux $HOME
+        cp .tmux.conf $HOME
+        ;;
+    vim)
+        cp .vimrc $HOME
+        ;;
+    bash)
+        cp .bashrc $HOME
+        ;;
+    i3)
+        cp .i3 $HOME/.config/i3/config
+        git clone https://github.com/shikherverma/i3lock-multimonitor.git
+        cp -r i3lock-multimonitor ~/.i3
+        chmod +x ~/.i3/i3lock-multimonitor/lock
+        apt install scrot
+        ;;
+esac;
 
-cp -r $CONF_FOLDER/bin $HOME
-for i in `ls $HOME/bin`; do chmod +x $HOME/bin/$i; done;
-
-cp -r $CONF_FOLDER/.tmux $HOME
-cp $CONF_FOLDER/.tmux.conf $HOME
-cp $CONF_FOLDER/.vimrc $HOME
-cp $CONF_FOLDER/.bashrc $HOME
-cp $CONF_FOLDER/.i3 $HOME/.config/i3/config
-
-git clone https://github.com/shikherverma/i3lock-multimonitor.git
-mv i3lock-multimonitor ~/.i3
-chmod +x ~/.i3/i3lock-multimonitor/lock
-
-apt install scrot
