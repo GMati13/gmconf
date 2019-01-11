7f02fd56c8f6318de26ad5334fe6f39e2736ab68
Mati Green <mati.green.13@gmail.com>
2019-01-11 14:37:53 +0200

    qutebrowser

--- qutebrowser.py
+++ qutebrowser.py
@@ -1,9 +1,17 @@
-config.bind('<Alt-h>', 'back')
-config.bind('<Alt-l>', 'forward')
-config.bind('<Alt-j>', 'tab-prev')
-config.bind('<Alt-k>', 'tab-next')
+config.bind('<Alt-h>', 'tab-prev')
+config.bind('<Alt-l>', 'tab-next')
+config.bind('<Alt-j>', 'back')
+config.bind('<Alt-k>', 'forward')
 
-c.url.start_pages = ["https://google.com/"]
-c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
+c.url.start_pages      = ["https://google.com/"]
+c.url.searchengines    = {"DEFAULT": "https://google.com/search?q={}"}
+c.editor.command       = ["urxvt", "-e", "vim", "{file}", "-c", "normal {line}G{column0}l"]
+c.session.default_name = "default"
 
-c.editor.command = ["urxvt", "-e", "vim", "{file}", "-c", "normal {line}G{column0}l"]
+c.aliases = {
+    "w":         "session-save",
+    "q":         "quit",
+    "wq":        "quit --save",
+    "translate": "open https://translate.google.com/#view=home&op=translate&sl=en&tl=ru&text="
+}
+
