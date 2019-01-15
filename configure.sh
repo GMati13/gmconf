ed06a6e58ae82a6b94150645d710c9d6163223e1
Mati Green <mati.green.13@gmail.com>
2019-01-16 00:00:46 +0200

    updated configure.sh. Added more custom settings for git

--- configure.sh
+++ configure.sh
@@ -1,45 +1,32 @@
 case "$1" in
-    all)
-        cp -r bin $HOME
-        for i in `ls $HOME/bin`; do chmod +x $HOME/bin/$i; done;
-        cp -r .tmux $HOME
-        cp .tmux.conf $HOME
-        cp .vimrc $HOME
-        cp .bashrc $HOME
-        cp .i3 $HOME/.config/i3/config
-        git clone https://github.com/shikherverma/i3lock-multimonitor.git
-        cp -r i3lock-multimonitor ~/.i3
-        chmod +x ~/.i3/i3lock-multimonitor/lock
-        cp background.png $HOME/.i3/i3lock-multimonitor/img
-        apt install scrot
-        cp qutebrowser.py $HOME/.config/qutebrowser/config.py
+bashrc)
+    if [ -z "$2" ]; then
+        cp bashrc ~/.bashrc
+    else
+        cp bashrc-$2 ~/.bashrc-$2
+    fi
+    ;;
+source)
+    echo "source ~/.bashrc-$2" >> ~/.bashrc
+    ;;
+install)
+    case "$2" in
+    plugin)
+        case "$3" in
+        git-prompt)
+            git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+            ;;
+        *)
+            echo unknown option \"$3\"
+            ;;
+        esac
         ;;
-    bin)
-        cp -r bin $HOME
-        for i in `ls $HOME/bin`; do chmod +x $HOME/bin/$i; done;
+    *)
+        echo unknown option \"$2\"
         ;;
-    tmux)
-        cp -r .tmux $HOME
-        cp .tmux.conf $HOME
-        ;;
-    vim)
-        cp .vimrc $HOME
-        ;;
-    bash)
-        cp .bashrc $HOME
-        ;;
-    browser)
-        cp qutebrowser.py $HOME/.config/qutebrowser/config.py
-        ;;
-    i3)
-        cp .i3 $HOME/.config/i3/config
-        git clone https://github.com/shikherverma/i3lock-multimonitor.git
-        cp -r i3lock-multimonitor ~/.i3
-        chmod +x ~/.i3/i3lock-multimonitor/lock
-        apt install scrot
-        ;;
-    i3-config)
-        cp .i3 $HOME/.config/i3/config
-        ;;
-esac;
-
+    esac
+    ;;
+*)
+    echo unknown option \"$1\"
+    ;;
+esac
