e3dc923bf7243626475248135ab6b6ee87e79d91
GMati13 <mati.green.13@gmail.com>
2019-01-16 14:04:37 +0200

    fix ycm option completer

--- scripts/ycm.sh
+++ scripts/ycm.sh
@@ -1,7 +1,7 @@
 case "$1" in
     install)
         apt install build-essential cmake python3-dev
-        ~/.vim/plugged/YouCompleteMe/install.py --$4-completer
+        ~/.vim/plugged/YouCompleteMe/install.py --$2-completer
         ;;
     *)
         echo $0: unknown option \"$1\"
